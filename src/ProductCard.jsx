a8591b83db95cab8f5616d34ea508a1c41469770
mdbicode <mdbahrulilmi@gmail.com>
2025-07-14 23:10:56 +0700

    add detail and a few list

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,19 +1,21 @@
-export default function ProductCard()
+import ProductDetail from "./ProductDetail"
+
+export default function ProductCard({onClick, product})
 {
     return(
-    <div class="max-w-xs bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-        <a href="#" className="align-center flex justify-center">
-            <img className="rounded-t-lg max-h-[200px]" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
+    <div key={product.key} onClick={onClick} className="max-w-xs bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 cursor-pointer">
+        <a href="#"className="align-center flex justify-center">
+            <img className="rounded-t-lg max-h-[200px]" src={product.image} alt="" />
         </a>
         <div className="p-5">
             <a href="#">
-                <h5 class="mb-1 text-lg font-bold tracking-tight text-gray-900 dark:text-white ">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h5>
+                <h5 className="mb-1 text-lg font-bold tracking-tight text-gray-900 dark:text-white ">{product.title}</h5>
             </a>
             <div className="bg-orange-100 px-[4px] w-fit mb-5">
-                <div class="font-bold text-sm text-orange-600 dark:text-orange-600">Category</div>
+                <div className="font-bold text-sm text-orange-600 dark:text-orange-600">{product.category}</div>
                 </div>
-            <p class="mb-3 font-bold text-lg text-gray-700 dark:text-gray-400">109.95</p>
-            <button type="button" class="w-full hover:cursor-pointer text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">ADD TO CART</button>
+            <p className="mb-3 font-bold text-lg text-gray-700 dark:text-gray-400">{product.price}</p>
+            <button type="button" className="w-full cursor-pointer text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={onClick}>SHOW DETAIL</button>
         </div>
     </div>
     )
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,38 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import ProductCard from "./ProductCard";
+import ProductDetail from "./ProductDetail";
+
 
 export default function ProductList()
 {
+    const [products, setProducts] = useState([])
+    const [open, setOpen] = useState(false)
+    const [selectedProduct, setSelectedProduct] = useState(null)
+
+    useEffect(()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then((res) => {
+            return res.json();
+        })
+        .then((data)=>{ 
+            setProducts(data)
+        })
+    })
+
     return(
         <>
-        <ProductCard/>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} onClick={() => {
+                    setSelectedProduct(product)
+                    setOpen(true)
+                }}/>
+            ))
+        }
+        </div>
+        <ProductDetail product={selectedProduct} open={open} setOpen={setOpen} />
         </>
     )
 }
